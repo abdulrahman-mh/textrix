c4659bcf5368549f43428df5222eb5a97a242737
Josh Trung <86184504+ConsecrateHQ@users.noreply.github.com>
2025-08-31 11:19:13 +0700

    Added a simple SmartReplacement feature

--- dev/index.ts
+++ dev/index.ts
@@ -1,49 +1,70 @@
-import '../src/styles/textrix.css'; // Default Textrix themes
-import { Editor } from '../src/Editor';
-import { BubbleMenu, Media, FloatingMenu, CodeBlock, Emoji } from '../src/features';
-import { uploadImage } from './helpers/uploadImage';
-import { fetchMediaEmbedData } from './helpers/fetchMediaEmbedData';
-import { buildSchema, generateHTML } from '../src';
+import "../src/styles/textrix.css"; // Default Textrix themes
+import { Editor } from "../src/Editor";
+import {
+  BubbleMenu,
+  Media,
+  FloatingMenu,
+  CodeBlock,
+  Emoji,
+  SmartReplacement,
+} from "../src/features";
+import { uploadImage } from "./helpers/uploadImage";
+import { fetchMediaEmbedData } from "./helpers/fetchMediaEmbedData";
+import { buildSchema, generateHTML } from "../src";
 
 // Initialize Editor
 const editor = new Editor({
   element: document.body,
   features: [
-    BubbleMenu.configure({bold: false}),
+    BubbleMenu.configure({ bold: false }),
     Media.configure({ fetchMediaEmbedData }),
     FloatingMenu,
     CodeBlock,
     Emoji,
+    SmartReplacement,
   ],
 });
 
 // Button creation helper function
 const createButton = (text: string, onClick: () => void) => {
-  const button = document.createElement('button');
+  const button = document.createElement("button");
   button.innerText = text;
   button.onclick = onClick;
   return button;
 };
 
-const buttonContainer = document.createElement('div');
-buttonContainer.className = 'button-container';
+const buttonContainer = document.createElement("div");
+buttonContainer.className = "button-container";
 
 // Add buttons to container
 const buttons = [
-  { text: 'Export JSON', action: () => console.log(JSON.stringify(editor.getJSON())) },
-  { text: 'Export HTML', action: () => console.log(editor.getHTML({ stripHeadlines: false })) },
-  { text: 'Export Metadata', action: () => console.log(JSON.stringify(editor.getMetadata())) },
   {
-    text: 'Export HTML (Server)',
+    text: "Export JSON",
+    action: () => console.log(JSON.stringify(editor.getJSON())),
+  },
+  {
+    text: "Export HTML",
+    action: () => console.log(editor.getHTML({ stripHeadlines: false })),
+  },
+  {
+    text: "Export Metadata",
+    action: () => console.log(JSON.stringify(editor.getMetadata())),
+  },
+  {
+    text: "Export HTML (Server)",
     action: () => {
       const schema = buildSchema();
-      const html = generateHTML(editor.getJSON(), schema, { stripHeadlines: false });
+      const html = generateHTML(editor.getJSON(), schema, {
+        stripHeadlines: false,
+      });
       console.log(html);
     },
   },
 ];
 
-buttons.forEach(({ text, action }) => buttonContainer.appendChild(createButton(text, action)));
+buttons.forEach(({ text, action }) =>
+  buttonContainer.appendChild(createButton(text, action))
+);
 
 // Append button container to the body
 document.body.appendChild(buttonContainer);
--- src/features/common.ts
+++ src/features/common.ts
@@ -1,8 +1,9 @@
 /**
  * Common Features packed under UMD
  */
-export * from './media';
-export * from './bubbleMenu';
-export * from './floatingMenu';
-export * from './codeBlock';
-export * from './emoji';
+export * from "./media";
+export * from "./bubbleMenu";
+export * from "./floatingMenu";
+export * from "./codeBlock";
+export * from "./emoji";
+export * from "./smartReplacement";
